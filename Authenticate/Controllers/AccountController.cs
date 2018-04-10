05092b140f6b2becef4704597521883035c11426
abhishek.chouksey <abhishek.chouksey@drivedominion.com>
2018-04-10 14:07:26 -0400

    Auto stash before merge of "master" and "origin/master"

---
+++ Authenticate/Controllers/AccountController.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Authenticate.Data;
+using Microsoft.AspNetCore.Authorization;
+
+namespace Authenticate.Controllers
+{
+    [Route("[controller]/[action]")]
+    [Authorize]
+    public class AccountController : Authenticate.Infrastructure.ControllerBase
+    {
+        private readonly SignInManager<ApplicationUser> _signInManager;
+        private readonly ILogger _logger;
+
+        public AccountController(SignInManager<ApplicationUser> signInManager, ILogger<AccountController> logger)
+        {
+            _signInManager = signInManager;
+            _logger = logger;
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Logout()
+        {
+            await _signInManager.SignOutAsync();
+            _logger.LogInformation("User logged out.");
+            return RedirectToPage("/Index");
+        }
+    }
+}
